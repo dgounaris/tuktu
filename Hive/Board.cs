047630f1c5d22fe12a010fca0680d15c21971612
Dimitrios Gounaris <dgounaris@microsoft.com>
2024-12-02 20:55:30 +0200

    Refactor board to not represent empty spaces

--- Hive/Board.cs
+++ Hive/Board.cs
@@ -5,8 +5,7 @@ namespace Hive;
 
 public class Board
 {
-    private IPiece?[,] _board = new IPiece?[15, 15];
-    private int _offset = 7;
+    private List<IPiece> _pieces = new List<IPiece>();
 
     public void Set(IPiece piece)
     {
@@ -15,34 +14,25 @@ public class Board
             throw new ArgumentException("Piece position is null");
         }
 
-        _board[piece.Position.Q + _offset, piece.Position.R + _offset] = piece;
+        _pieces.Add(piece);
     }
 
     public IPiece? Get(Position position)
     {
-        return _board[position.Q + _offset, position.R + _offset];
+        // last or default will come in handy when we have beetles functionality working
+        return _pieces.LastOrDefault(it => it.Position.Q == position.Q && it.Position.R == position.R);
     }
 
     public void Remove(Position position)
     {
-        _board[position.Q + _offset, position.R + _offset] = null;
+        // last will come in handy when we have beetles functionality working
+        _pieces.RemoveAt(_pieces.FindLastIndex(it => it.Position == position));
     }
     
     public IPiece? GetPiece(bool color, char pieceId, int pieceNumber)
     {
-        foreach (var boardTile in _board)
-        {
-            if (boardTile is null)
-            {
-                continue;
-            }
-            if (boardTile.Color == color && boardTile.GetPieceIdentifier() == pieceId && boardTile.PieceNumber == pieceNumber)
-            {
-                return boardTile;
-            }
-        }
-
-        return null;
+        return _pieces.SingleOrDefault(it => 
+            it.Color == color && it.GetPieceIdentifier() == pieceId && it.PieceNumber == pieceNumber);
     }
 
     public IEnumerable<Position> GetSurroundingPositions(Position position)
@@ -73,11 +63,12 @@ public class Board
 
     public bool IsPositionInitializable(Position position, bool color)
     {
-        if (_board[position.Q + _offset, position.R + _offset] is not null)
+        if (_pieces.Any(it => it.Position!.Q == position.Q && it.Position.R == position.R))
         {
             return false;
         }
-        var existingBoardPieces = _board.Cast<IPiece?>().Count(it => it is not null);
+        
+        var existingBoardPieces = _pieces.Count;
         if (existingBoardPieces < 2)
         {
             return true;
@@ -87,15 +78,17 @@ public class Board
         var surroundingOtherColor = 0;
         foreach (var surroundingPosition in surroundingPositions)
         {
-            if (_board[surroundingPosition.Q + _offset, surroundingPosition.R + _offset] is null)
+            var pieceOnPosition = _pieces.FirstOrDefault(it =>
+                it.Position!.Q == surroundingPosition.Q && it.Position.R == surroundingPosition.R);
+            if (pieceOnPosition is null)
             {
                 continue;
             }
-            else if (_board[surroundingPosition.Q + _offset, surroundingPosition.R + _offset]?.Color == color)
+            else if (pieceOnPosition.Color == color)
             {
                 surroundingSameColor++;
             }
-            else if (_board[surroundingPosition.Q + _offset, surroundingPosition.R + _offset]?.Color != color)
+            else if (pieceOnPosition.Color != color)
             {
                 surroundingOtherColor++;
             }
@@ -129,32 +122,33 @@ public class Board
     public void Print()
     {
         Console.WriteLine("Board state:");
-        if (!_board.Cast<IPiece?>().Any(it => it is not null))
+        if (_pieces.Count == 0)
         {
             Console.WriteLine("Empty board");
             return;
         }
-        var westMostPos = _board.Cast<IPiece?>().Where(it => it is not null).MinBy(it => it!.Position!.Q)!.Position!.Q + _offset;
-        var northMostPos = _board.Cast<IPiece?>().Where(it => it is not null).MinBy(it => it!.Position!.R)!.Position!.R + _offset;
-        var eastMostPos = _board.Cast<IPiece?>().Where(it => it is not null).MaxBy(it => it!.Position!.Q)!.Position!.Q + _offset;
-        var southMostPos = _board.Cast<IPiece?>().Where(it => it is not null).MaxBy(it => it!.Position!.R)!.Position!.R + _offset;
+        var westMostPos = _pieces.MinBy(it => it!.Position!.Q)!.Position!.Q;
+        var northMostPos = _pieces.MinBy(it => it!.Position!.R)!.Position!.R;
+        var eastMostPos = _pieces.MaxBy(it => it!.Position!.Q)!.Position!.Q;
+        var southMostPos = _pieces.MaxBy(it => it!.Position!.R)!.Position!.R;
         
         for (var r = northMostPos - 2; r <= southMostPos + 2; r++)
         {
-            if (Math.Abs(r - _offset) % 2 == 1)
+            if (Math.Abs(r) % 2 == 1)
             {
                 Console.Write("  ");
             }
             for (var q = westMostPos - 2; q <= eastMostPos + 2; q++)
             {
-                if (_board[q, r] is null)
+                var pieceOnLocation = _pieces.FirstOrDefault(it => it.Position!.Q == q && it.Position.R == r);
+                if (pieceOnLocation is null)
                 {
                     Console.Write("  .  ");
                 }
                 else
                 {
                     string colorAsStr;
-                    if (_board[q, r]!.Color)
+                    if (pieceOnLocation.Color)
                     {
                         colorAsStr = "w";
                     }
@@ -162,7 +156,7 @@ public class Board
                     {
                         colorAsStr = "b";
                     }
-                    Console.Write($" {colorAsStr}{_board[q, r]!.GetPieceIdentifier()}{_board[q, r]!.PieceNumber} ");
+                    Console.Write($" {colorAsStr}{pieceOnLocation.GetPieceIdentifier()}{pieceOnLocation.PieceNumber} ");
                 }
             }
             Console.WriteLine();
